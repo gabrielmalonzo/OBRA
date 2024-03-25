177bc8498e9f120d90708ab29798839194368fdb
gabrielmalonzo <107390975+gabrielmalonzo@users.noreply.github.com>
2024-03-25 13:28:03 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Obra</title>
+</head>
+<body>
+    <?php
+        include 'nav_bar.php';
+    ?>
+    <div class="container">
+ 
+        <div class="left">
+            <div class="light">
+                <h1 class="content">
+                    WELCOME HERE BLUE COLLAR!<BR>
+                    TRY THIS NEW WAY TO<BR>
+                    FIND A POSSIBLE JOB!
+                </h1>
+
+                <p class="content-1">Here blue collar worker is our priority <br> 
+                    submit your application here.
+                </p>
+
+                <div class="background">
+                    <a href="APPLY">Apply now!</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="right">
+
+        </div>
+
+    </div>
+</body>
+</html>
