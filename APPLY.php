54e2099f024bae3aa631f560c13c30df51ec3ef4
gabrielmalonzo <107390975+gabrielmalonzo@users.noreply.github.com>
2024-03-25 18:41:46 +0800

    Add files via upload

---
+++ APPLY.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WORKER's Page</title>
+    <style>
+        body{
+            margin: 0px;
+            padding: 0px;
+            display: flex;
+            justify-content: center;
+            align-content: center;
+            background-color: green;
+        }
+        button{
+            width: 200px;
+            height: 100px;
+        }
+    </style>   
+</head>
+<body>
+        <button><a href="index.php">BACK!</a></button>
+</body>
+</html>
---
+++ about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+    <style>
+        body{
+            margin: 0px;
+            padding: 0px;
+            background: #EFEFEF; 
+        }
+    </style>
+</head>
+<body>
+    <?php
+        include 'nav_bar.php';
+    ?>
+    about page
+</body>
+</html>
